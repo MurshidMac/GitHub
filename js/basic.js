6fe4de4d708b96e84c708a4678e9a72508143843
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-18 10:59:22 +0530

    added js configure value

--- js/basic.js
+++ js/basic.js
@@ -1,3 +1,30 @@
 function Ouput(){
     console.log("I am Murshid");
 }
+// JS DataType
+// Java Script Has basically five DataType
+// these are five major Type of DataType available in  Javascript
+        // Number
+        // String 
+        // Boolean
+        // Undefinded
+        // Null 
+
+// TO check all the available datatype in js
+function dataTypes(){
+    
+    return () => {
+            let number = Number.MAX_VALUE;
+            console.log(number);
+            number= Number.MIN_VALUE;
+            console.log(number);
+            let value= string.MAX_VALUE;   // this will 
+            console.log(value);
+            value=string.MIN_VALUE;
+            console.log(value);
+            let definetest= undefined;
+            console.log(definetest);
+            // there is a define function in javascript as well
+            
+    }
+}
